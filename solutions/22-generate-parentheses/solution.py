dde29f4139ce5cba790c8407ac9c2535bc34e668
Irfan Khan <khandev1211@gmail.com>
2026-07-19 14:59:19 +0500

    Add solution: Generate Parentheses

--- solutions/22-generate-parentheses/solution.py
+++ solutions/22-generate-parentheses/solution.py
@@ -1,13 +1,13 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def backtrack(open_p, close_p, current):
-            if len(current) == 2 * n:
-                result.append(current)
+        def backtrack(open_p, close_p, path):
+            if len(path) == 2 * n:
+                result.append(path)
                 return
             if open_p < n:
-                backtrack(open_p + 1, close_p, current + '(')
+                backtrack(open_p + 1, close_p, path + '(')
             if close_p < open_p:
-                backtrack(open_p, close_p + 1, current + ')')
+                backtrack(open_p, close_p + 1, path + ')')
         result = []
         backtrack(0, 0, '')
         return result
