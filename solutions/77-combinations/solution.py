342ace2beac17b4a33b9edbe6a210ace286e645f
Irfan Khan <khandev1211@gmail.com>
2026-07-29 00:29:00 +0500

    Add solution: Combinations (Medium, Python3)

---
+++ solutions/77-combinations/solution.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def backtrack(start, current):
+            if len(current) == k:
+                result.append(current.copy())
+                return
+            for i in range(start, n + 1):
+                current.append(i)
+                backtrack(i + 1, current)
+                current.pop()
+        
+        result = []
+        backtrack(1, [])
+        return result
