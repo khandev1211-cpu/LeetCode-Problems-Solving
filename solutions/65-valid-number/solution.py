0b35e5ba4bfde7ff790f197beb986378f7b237ae
Irfan Khan <khandev1211@gmail.com>
2026-07-28 16:13:43 +0500

    Add solution: Valid Number (Hard, Python3)

--- solutions/65-valid-number/solution.py
+++ solutions/65-valid-number/solution.py
@@ -1,3 +1,25 @@
 class Solution:
     def isNumber(self, s: str) -> bool:
+        seen_digit = False
+        seen_dot = False
+        seen_exponent = False
         
+        for i, char in enumerate(s):
+            if char.isdigit():
+                seen_digit = True
+            elif char in ['+', '-']:
+                if i > 0 and s[i-1] not in ['e', 'E']:
+                    return False
+            elif char in ['e', 'E']:
+                if seen_exponent or not seen_digit:
+                    return False
+                seen_exponent = True
+                seen_digit = False
+            elif char == '.':
+                if seen_dot or seen_exponent:
+                    return False
+                seen_dot = True
+            else:
+                return False
+        
+        return seen_digit
