2a2f98cda7fb0d55bc1b5fbedeae33c8bfde3b8d
Irfan Khan <khandev1211@gmail.com>
2026-08-05 00:10:32 +0500

    Add solution: Path Sum (Easy, Python3)

---
+++ solutions/112-path-sum/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        if not root.left and not root.right:
+            return root.val == targetSum
+        targetSum -= root.val
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
