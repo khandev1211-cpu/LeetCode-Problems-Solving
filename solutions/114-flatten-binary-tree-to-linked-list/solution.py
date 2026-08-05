f1baf3f35965a53f8dc193950283f58fe59fdcd3
Irfan Khan <khandev1211@gmail.com>
2026-08-06 00:18:00 +0500

    Add solution: Flatten Binary Tree to Linked List (Medium, Python3)

---
+++ solutions/114-flatten-binary-tree-to-linked-list/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        if not root:
+            return
+        node = root
+        while node:
+            if node.left:
+                rightmost = node.left
+                while rightmost.right:
+                    rightmost = rightmost.right
+                rightmost.right = node.right
+                node.right = node.left
+                node.left = None
+            node = node.right
